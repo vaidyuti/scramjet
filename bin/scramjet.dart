cee641d1602dd6a0eb730ef89e63fa8c43983ec6
Rithvik Nishad <rithvikn2001@gmail.com>
2022-10-16 19:19:58 +0530

    accomplish input layer

--- bin/scramjet.dart
+++ bin/scramjet.dart
@@ -1,5 +1,33 @@
+import 'package:dotenv/dotenv.dart';
+import 'package:mqtt5_client/mqtt5_client.dart';
+import 'package:mqtt5_client/mqtt5_server_client.dart';
+import 'package:scramjet/prosumer_data.dart';
 import 'package:scramjet/scramjet.dart' as scramjet;
 
-void main(List<String> arguments) {
-  print('Hello world: ${scramjet.calculate()}!');
+void main(List<String> arguments) async {
+  final env = DotEnv(includePlatformEnvironment: true)..load();
+  print(env['MQTT_SERVER']);
+
+  final mqttServer = env['MQTT_SERVER'] ?? 'tcp://localhost';
+  final client = MqttServerClient(mqttServer, '');
+
+  await client.connect();
+  print('Connected to MQTT Server');
+
+  client.subscribe('prosumer/+/data', MqttQos.atMostOnce);
+
+  client.updates.listen((event) {
+    event.forEach((message) {
+      final topic = message.topic!;
+      final payload = MqttUtilities.bytesToStringAsString(
+          (message.payload as MqttPublishMessage).payload.message!);
+
+      final prosumerId = topic.split('/')[1];
+      final prosumerData = ProsumerData.fromJson(payload);
+
+      scramjet.addEvent(prosumerId, prosumerData);
+
+      print('[$topic] : $payload');
+    });
+  });
 }
--- lib/scramjet.dart
+++ lib/scramjet.dart
@@ -1,3 +1,16 @@
-int calculate() {
-  return 6 * 7;
+import 'package:scramjet/prosumer_data.dart';
+
+final Map<String, ProsumerData> prosumerStates = Map.from({});
+
+void addEvent(String prosumerId, ProsumerData data) {
+  prosumerStates.update(
+    prosumerId,
+    (value) => data,
+    ifAbsent: () {
+      // print("New prosumer '$prosumerId' detected.");
+      return data;
+    },
+  );
+
+  // TODO: re-evaluate market price with new prosumer states
 }
